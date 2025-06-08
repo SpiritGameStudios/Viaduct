9ff877b2e6ee827cadb8f7fd61a9780491536ec0
worldwidepixel <worldwidepxl@gmail.com>
2025-06-07 23:03:05 -0700

    fix: Small little safety concern of mine

--- src/routes/[id]/+page.server.ts
+++ src/routes/[id]/+page.server.ts
@@ -1,9 +1,13 @@
 import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from '../$types';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
-	redirect(
-		303,
-		'https://discord.com/oauth2/authorize?scope=bot+applications.commands&client_id=' + params.id
-	);
+	if (params.id.length >= 18 && params.id.length <= 20) {
+		redirect(
+			303,
+			'https://discord.com/oauth2/authorize?scope=bot+applications.commands&client_id=' + params.id
+		);
+	} else {
+		redirect(303, '/');
+	}
 };
