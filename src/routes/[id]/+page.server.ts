291e5667547fd2651a5874ad380f11a6697be9d9
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2025-06-08 00:23:21 -0700

    fix: Auto-permissions!

--- src/routes/[id]/+page.server.ts
+++ src/routes/[id]/+page.server.ts
@@ -5,7 +5,7 @@ export const load: PageServerLoad = async ({ params }) => {
 	if (params.id.length >= 18 && params.id.length <= 20) {
 		redirect(
 			303,
-			'https://discord.com/oauth2/authorize?scope=bot+applications.commands&client_id=' + params.id
+			'https://discord.com/oauth2/authorize?scope=bot+applications.commands&permissions=536938496&client_id=' + params.id
 		);
 	} else {
 		redirect(303, '/');
